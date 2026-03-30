4203509e93058fc48cec90527174f4b5962c1f76
paulomonteiro1976 <paulo.monteiro@simplify-hospitality.com>
2026-03-30 22:35:44 +0200

    trigger ci pipeline2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
-resource "azurerm_resource_group" "rg1" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_storage_account" "storage1" {
+resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
