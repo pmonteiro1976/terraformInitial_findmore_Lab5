7cbb5752b1fa2b6f7c2b8096c43a63951ba2748a
paulomonteiro1976 <paulo.monteiro@simplify-hospitality.com>
2026-03-30 22:19:32 +0200

    docs: update resource group names and backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,11 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "existing-backend-rg"
-    storage_account_name = "existingstorageaccount"
+
+    resource_group_name  = "rg_lab"
+    storage_account_name = "fmacademystorage1234"
     container_name       = "tfstate"
-    key                  = "student-pauloMonteiro.tfstate"
+
+    key = "paulomonteiro-cicd.tfstate"
+
   }
 }
